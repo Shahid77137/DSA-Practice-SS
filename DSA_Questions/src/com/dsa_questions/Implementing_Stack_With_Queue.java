0eefba973d8f922c094300cdcd8db3ced3a51fc0
Soumit-Das <soumitdas500@gmail.com>
2023-10-10 11:59:48 +0530

    Stack Question 2

---
+++ DSA_Questions/src/com/dsa_questions/Implementing_Stack_With_Queue.java
@@ -0,0 +1,86 @@
+public class Implementing_Stack_With_Queue{
+
+    Stack<Integer> s1 = new Stack<>();
+
+    Stack<Integer> s2 = new Stack<>();
+
+
+    public void push(int x) {
+
+        s1.push(x);
+
+    }
+
+    public int pop() {
+
+        if(!s2.empty()){
+
+            return s2.pop();
+
+        }else{
+
+            while(!s1.empty()){
+
+                int temp = s1.pop();
+
+                s2.push(temp);
+
+            }
+
+            return s2.pop();
+        }
+
+
+    }
+
+    public int peek() {
+
+        if(!s2.empty()){
+
+            return s2.peek();
+
+        } else{
+
+            while(!s1.empty()){
+
+                int temp = s1.pop();
+
+                s2.push(temp);
+
+            }
+
+            return s2.peek();
+
+        }
+
+    }
+
+    public boolean empty() {
+
+        if(s1.empty() == false){
+
+            if(s2.empty()){
+
+                while(!s1.empty()){
+
+                    int temp = s1.pop();
+
+                    s2.push(temp);
+
+                }
+
+            }
+
+            return false;
+
+        }
+
+        else{
+
+            return s2.isEmpty();
+
+        }
+
+    }
+
+}
