4bf90e19e4f6cbf20a24453eb23195584e0fd863
Soumit-Das <soumitdas500@gmail.com>
2023-10-10 12:25:44 +0530

    Easy Stack - Valid Parenthesis dsa question added

---
+++ Soumit_DSA_sheet/out/production/Soumit_DSA_sheet/Balanced_Parenthesis.java
@@ -0,0 +1,63 @@
+public class Balanced_Parenthesis {
+
+    public boolean isValid(String s) {
+
+        char[] ch = s.toCharArray();
+
+        Stack<Character> stack = new Stack<>();
+
+        if(ch[0] == ')' || ch[0] == '}' || ch[0] == ']'){
+
+            return false;
+
+        }
+
+        for(int i = 0;i < ch.length;i++){
+
+
+            if(ch[i] == '(' || ch[i] == '{' || ch[i] == '['){
+
+                stack.push(ch[i]);
+
+            }else{
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if(stack.peek() == '(' && ch[i] == ')'){
+
+                    stack.pop();
+
+
+                }else if(stack.peek() == '{' && ch[i] == '}'){
+
+                    stack.pop();
+
+                }else if(stack.peek() == '[' && ch[i] == ']'){
+
+                    stack.pop();
+
+                }
+                else{
+
+                    return false;
+
+                }
+
+            }
+
+        }
+
+
+        if(stack.isEmpty()){
+
+            return true;
+
+        }else{
+
+            return false;
+
+        }
+
+    }
+
+}
